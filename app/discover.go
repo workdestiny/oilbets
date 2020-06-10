1f0a16e5761b7831b5d9c3c1ea7cf3158ec0853f
workdestiny <workdestiny@hotmail.com>
2020-06-10 16:38:22 +0700

    test

--- app/discover.go
+++ app/discover.go
@@ -1,10 +1,13 @@
 package app
 
 import (
+	"log"
+
 	"github.com/moonrhythm/hime"
 )
 
 func discoverGetHandler(ctx *hime.Context) error {
+	log.Println("111")
 
 	return ctx.View("app/discover", page(ctx))
 }
